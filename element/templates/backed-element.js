a446a4373af67f3f8943c90c4afaa787fad4dde2
Glenn Vandeuren <vandeurenglenn@gmail.com>
2017-03-21 01:36:46 +0100

    :mega: Update syntax for current Backed version

--- element/templates/backed-element.js
+++ element/templates/backed-element.js
@@ -1,6 +1,20 @@
 'use strict';
+/**
+ * A trying to be useful element!
+ * @extends HTMLElement
+ */
 export default Backed(class <%= className %> extends HTMLElement {
   constructor() {
+    // Only super is called here
+    // avoid using a constructor
+    // more info @ https://github.com/VandeurenGlenn/backed/issues/4
     super();
+  }
+  /**
+   * Called when Backed is finished setting up,
+   * https://github.com/VandeurenGlenn/backed/wiki/Lifecycle-callback-methods
+   */
+  ready() {
+    // This is where your code should go
   }
 });
